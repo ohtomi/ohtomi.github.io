27bfac88970c09b67ff48ea0d9f77b8aea3d6b2e
ohtomi <ohtomi.kenichi@gmail.com>
2018-04-04 21:41:22 +0900

    rm website/static/img/oss_logo.png

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
 class Footer extends React.Component {
@@ -77,19 +70,8 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          className="fbOpenSource">
-          <img
-            src={this.props.config.baseUrl + 'img/oss_logo.png'}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">
-          Copyright &copy; {currentYear} Facebook Inc.
+          Copyright &copy; {currentYear} Kenichi Ohtomi
         </section>
       </footer>
     );
