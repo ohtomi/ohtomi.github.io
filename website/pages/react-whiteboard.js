2a2d6611bf9430e91d3a74cf47b0cf24a39ee7f2
ohtomi <ohtomi.kenichi@gmail.com>
2018-04-09 21:56:02 +0900

    Add demo script for whiteboard [WIP]

--- website/pages/react-whiteboard.js
+++ website/pages/react-whiteboard.js
@@ -1,10 +1,20 @@
 const React = require('react')
 
+const CompLibrary = require('../core/CompLibrary.js')
+const Container = CompLibrary.Container
+
 class Demo extends React.Component {
     render() {
         return (
             <div>
-                TODO
+                <div className="mainContainer">
+                    <Container id="example">
+                        <h2>react-whiteboard example</h2>
+                        TODO
+                    </Container>
+                </div>
+                <script src="/js/whiteboard/page.js">
+                </script>
             </div>
         )
     }
